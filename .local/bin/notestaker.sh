3b9e444c300ba4eaecfdc5f9d2af516d6b818b5e
hendry <h3ndry@users.noreply.github.com>
2024-06-13 10:38:55 +0200

    refactor(location): change location

--- .local/bin/notestaker.sh
+++ .local/bin/notestaker.sh
@@ -4,7 +4,7 @@ WEEK_NUMBER=$(date +%V)
 YEAR=$(date +%Y)
 MONTH=$(date +%m)
 
-NOTE_FILE_NAME="$HOME/workspace/my-notes/${WEEK_NUMBER}_${MONTH}_${YEAR}.md"
+NOTE_FILE_NAME="$HOME/workspace/my-notes/notes/${WEEK_NUMBER}_${MONTH}_${YEAR}.md"
 
 if [ ! -f $NOTE_FILE_NAME ]; then
   echo "## Notes | Year: ${YEAR} | Month: ${MONTH} | Week: ${WEEK_NUMBER}" > $NOTE_FILE_NAME
