a17bcf1bcfd5e6ca1caf1c48984f63d8422f7eb1
hendry <h3ndry@users.noreply.github.com>
2024-06-22 13:03:06 +0000

    feat(tmux): Integrate tmux into the quickmove script, significantly improving my workflow.

--- .local/bin/quickmove.sh
+++ .local/bin/quickmove.sh
@@ -3,34 +3,44 @@
 BASE='/home/simanga/'
 
 # for personal files and all that kind of staff I mess with
-items=`fd --max-depth=1 --type=d --base-directory=${BASE} . 'Code'`
-items+=$'\n'
+# items=`fd --max-depth=1 --type=d --base-directory=${BASE} . 'Code'`
+# items+=$'\n'
 items+=`fd --max-depth=1 --type=d  --base-directory=${BASE} . 'workspace'`
 items+=$'\n'
 items+=`fd --max-depth=1 --type=d  --base-directory=${BASE} . '.config'`
 items+=$'\n'
 items+=`fd --max-depth=1 --type=d  --base-directory=${BASE} . '.local/share/nvim/lazy'`
-items+=$'\n'
-items+=`fd --max-depth=1 --type=d  --base-directory=${BASE} . 'workspace/hackerthon'`
+# items+=$'\n'
+# items+=`fd --max-depth=1 --type=d  --base-directory=${BASE} . 'workspace/hackerthon'`
 
-items+=$'\n'
+# items+=$'\n'
 # for a company specific projects
-items+=`fd -H -I --type=directory --base-directory=/home/hendry/workspace/kriterion -E ".github" "\.git" | sed '\.git/' '' | sed '(^\w)' 'workspace/kriterion/$1'`
+# items+=`fd -H -I --type=directory --base-directory=/home/hendry/workspace/kriterion -E ".github" "\.git" | sed '\.git/' '' | sed '(^\w)' 'workspace/kriterion/$1'`
 items+=$'\n'
 
 
 
 FOLDER=`echo "$items" | rofi -dmenu`
 
-if [ -d "$FOLDER" ]; then
+    echo ${FOLDER}
+
+if [ -d "${BASE}${FOLDER}" ]; then
 	n="$(echo ${FOLDER} | sed 's/\/$//')"
 	n="$(echo ${n} | sed 's/\//->/g')"
 
-    echo ${n}
+	PROJECT_NAME="$(echo ${n} | sed 's/.*>//')"
 
-    # EXT=`fd -e py --base-directory=${BASE}${FOLDER}`
+	echo ${n}
+	echo ${PROJECT_NAME}
 
-    alacritty -T "neovim - $n" --class "NIDE"  -e nvim ${BASE}${FOLDER} --cmd "cd ${BASE}${FOLDER}"
+	cd ${BASE}${FOLDER}
+
+	tmux new-session -d -s ${PROJECT_NAME}
+
+	tmux send-keys -t ${PROJECT_NAME} "nvim ." Enter
+	tmux attach-session -t ${PROJECT_NAME}
+
+	alacritty -T "neovim - $n" --class "NIDE"  -e tmux attach-session -t ${PROJECT_NAME}
 fi
 
 # echo "$items" | fzf | xargs -I {} nvim {} --cmd 'cd {}'
