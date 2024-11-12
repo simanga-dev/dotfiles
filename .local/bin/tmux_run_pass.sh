585155e944a6dcbb28fc3e49047da75e1221af5b
Simanga Khoza <simanga-dev@users.noreply.github.com>
2024-11-12 15:04:31 +0200

    update to change to the last window on change

--- .local/bin/tmux_run_pass.sh
+++ .local/bin/tmux_run_pass.sh
@@ -8,7 +8,7 @@ if ! tmux list-windows -t default 2>/dev/null | grep -q "default"; then
     tmux new-window -t default -n  default
 fi
 
-tmux send-keys -t default:default "PASSWORD_STORE_ENABLE_EXTENSIONS=true pass fzf" Enter
+tmux send-keys -t default:default "fzf-pass -f -c" Enter
 
 tmux select-window -t default
 
