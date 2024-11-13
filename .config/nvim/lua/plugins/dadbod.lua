516ea0eceea2626f0f36a82c41652b05c8bab197
Simanga Khoza <simanga-dev@users.noreply.github.com>
2024-11-13 19:09:27 +0000

    update(hypr): More configuration on HyprlandB.

--- .config/nvim/lua/plugins/dadbod.lua
+++ .config/nvim/lua/plugins/dadbod.lua
@@ -24,7 +24,7 @@ return {
     vim.g.db_ui_use_nerd_fonts = 1
     vim.g.db_ui_default_query = 'SELECT * FROM "{table}" LIMIT 2;'
     vim.g.db_ui_use_nerd_fonts = 1
-    vim.g.db_ui_save_location = '~/workspace/my-notes/db_ui'
+    vim.g.db_ui_save_location = '~/Workspace/my-notes/db_ui'
     vim.g.db_ui_execute_on_save = 0
     vim.g.db_ui_win_position = 'right'
     vim.g.db_ui_tmp_query_location = '~/workspace/my-notes/db_ui'
