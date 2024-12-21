9cc41ef7f47a8fc4f66f4eb6354feac57e57d56c
Simanga Khoza <simanga-dev@users.noreply.github.com>
2024-12-21 14:54:26 +0200

    make sure

--- .config/nvim/lua/plugins/dadbod.lua
+++ .config/nvim/lua/plugins/dadbod.lua
@@ -27,7 +27,7 @@ return {
     vim.g.db_ui_save_location = '~/Workspace/my-notes/db_ui'
     vim.g.db_ui_execute_on_save = 0
     vim.g.db_ui_win_position = 'right'
-    vim.g.db_ui_tmp_query_location = '~/workspace/my-notes/db_ui'
+    vim.g.db_ui_tmp_query_location = '~/Workspace/my-notes/db_ui'
 
     vim.api.nvim_create_autocmd({ 'BufReadPost' }, {
       pattern = { '*.dbout' },
