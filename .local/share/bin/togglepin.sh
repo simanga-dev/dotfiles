7f7bd561d97ad5e0a5c9f293e012d49f7986fa18
Simanga Khoza <simanga-dev@users.noreply.github.com>
2025-03-16 16:14:32 +0200

    save

--- .local/share/bin/togglepin.sh
+++ .local/share/bin/togglepin.sh
@@ -1,23 +1,31 @@
 #!/usr/bin/env sh
 
-# enable float
+# Check if the active window is floating and pinned
 WinFloat=$(hyprctl -j clients | jq '.[] | select(.focusHistoryID == 0) | .floating')
 WinPinned=$(hyprctl -j clients | jq '.[] | select(.focusHistoryID == 0) | .pinned')
 
-## Only video player not other windo
-MediaPlayer=$(hyprctl activewindow -j | jq -r '.initialClass')
+# Get the class of the active window
+PiPCheck=$(hyprctl activewindow -j | jq -r 'select(.initialClass == "mpv")')
+PiPCheck=+$(hyprctl activewindow -j | jq -r 'select(.class == "zen") | select(.title == "Picture-in-Picture")')
 
-
-if [ "${WinFloat}" == "false" ] && [ "${WinPinned}" == "false" ] && [ "${MediaPlayer}" == "mpv" ]  ; then
+# Function to toggle floating and adjust the window size/position
+toggle_floating_and_resize() {
     hyprctl dispatch togglefloating active
     hyprctl dispatch resizeactive exact 489 275
     hyprctl dispatch moveactive exact 2000 856
     hyprctl dispatch pin
     hyprctl dispatch cyclenext
-else
-    if  [ "${MediaPlayer}" == "mpv" ] ; then
-        hyprctl dispatch togglefloating active
-        hyprctl dispatch fullscreenstate 2 3
-    fi
-fi
+}
 
+# Function to toggle floating and set fullscreen state
+toggle_floating_and_fullscreen() {
+    hyprctl dispatch togglefloating active
+    hyprctl dispatch fullscreenstate 2 3
+}
+
+# Main logic
+if [ "${WinFloat}" = "false" ] && [ "${WinPinned}" = "false" ] && [ -n "${PiPCheck}" ]; then
+    toggle_floating_and_resize
+elif  [ -n "${PiPCheck}" ]; then
+    toggle_floating_and_fullscreen
+fi
