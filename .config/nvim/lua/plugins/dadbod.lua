593590b99128327cc5d2d6921c56a756216cc9e9
Simanga Khoza <simanga-dev@users.noreply.github.com>
2025-04-02 03:52:45 +0200

    git commit "save"

--- .config/nvim/lua/plugins/dadbod.lua
+++ .config/nvim/lua/plugins/dadbod.lua
@@ -32,7 +32,10 @@ return {
     vim.api.nvim_create_autocmd({ 'BufReadPost' }, {
       pattern = { '*.dbout' },
       callback = function()
-        vim.api.nvim_exec2([[ exe ':resize 20' ]], {})
+        -- vim.api.nvim_exec2([[ exe ':resize 50' ]], {})
+        local screen_height = vim.o.lines
+        local half_height = math.floor(screen_height * 0.4)
+        vim.cmd('resize ' .. half_height)
         vim.g.db_ui_use_nerd_fonts = 1
       end,
     })
