caca1d3622efb22d6e94303b93b2b05652e11f58
Simanga Khoza <simanga-dev@users.noreply.github.com>
2025-04-07 10:23:53 +0200

    move away from telescope to picker

--- .config/nvim/lua/plugins/snacks-nvim.lua
+++ .config/nvim/lua/plugins/snacks-nvim.lua
@@ -1,16 +1,79 @@
 -- lazy.nvim
 return {
   'folke/snacks.nvim',
+  priority = 1000,
+  lazy = false,
+
   opts = {
     -- scroll = {
     --   -- your scroll configuration comes here
     --   -- or leave it empty to use the default settings
     --   -- refer to the configuration section below
     -- },
+    statuscolumn = {},
+    picker = {
+      -- your picker configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
     words = {
       -- your words configuration comes here
       -- or leave it empty to use the default settings
       -- refer to the configuration section below
+    },
+  },
+  keys = {
+    -- Top Pickers & Explorer
+    {
+      '<leader><space>',
+      function()
+        Snacks.picker.smart()
+      end,
+      desc = 'Smart Find Files',
+    },
+    {
+      '<leader>ff',
+      function()
+        Snacks.picker.files()
+      end,
+      desc = 'Find Files',
+    },
+    {
+      '<leader>/',
+      function()
+        Snacks.picker.grep()
+      end,
+      desc = 'Grep',
+    },
+    -- Grep
+    {
+      '<leader>sb',
+      function()
+        Snacks.picker.lines()
+      end,
+      desc = 'Buffer Lines',
+    },
+    {
+      '<leader>sB',
+      function()
+        Snacks.picker.grep_buffers()
+      end,
+      desc = 'Grep Open Buffers',
+    },
+    {
+      '<leader>sg',
+      function()
+        Snacks.picker.grep()
+      end,
+      desc = 'Grep',
+    },
+    {
+      '<leader>sw',
+      function()
+        Snacks.picker.grep_word()
+      end,
+      desc = 'Visual selection or word',
+      mode = { 'n', 'x' },
     },
   },
 }
