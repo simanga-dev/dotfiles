72b33cf9acdbd57439d77d620894f254e035a431
Simanga Khoza <simanga-dev@users.noreply.github.com>
2025-04-22 16:23:51 +0200

    update save

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -12,7 +12,7 @@ return {
       desc = '[T] Harpoon Add',
     },
     {
-      '<C-f>',
+      '<C-e>',
       function()
         local harpoon = require 'harpoon'
         harpoon.ui:toggle_quick_menu(harpoon:list())
@@ -20,43 +20,28 @@ return {
       desc = '[T] Harpoon Menu',
     },
     {
-      '<C-b>',
+      '<C-h>',
       function()
         require('harpoon'):list():select(1)
       end,
       desc = '[T] Harpoon GoTo 1',
     },
     {
-      '<C-c>',
+      '<C-t>',
       function()
         require('harpoon'):list():select(2)
       end,
       desc = '[T] Harpoon GoTo 2',
     },
     {
-      '<C-d>',
+      '<C-n>',
       function()
         require('harpoon'):list():select(3)
       end,
       desc = '[T] Harpoon GoTo 3',
     },
     {
-      '<C-e>',
-      function()
-        require('harpoon'):list():select(4)
-      end,
-      desc = '[T] Harpoon GoTo 2',
-    },
-
-    {
-      '<C-c>',
-      function()
-        require('harpoon'):list():select(3)
-      end,
-      desc = '[T] Harpoon GoTo 3',
-    },
-    {
-      '<C-d>',
+      '<C-s>',
       function()
         require('harpoon'):list():select(4)
       end,
@@ -66,14 +51,14 @@ return {
     {
       '<leadereader>>',
       function()
-        require('harpoon'):list():select(3)
+        require('harpoon'):list():prev()
       end,
       desc = '[T] Harpoon GoTo 3',
     },
     {
       '<leader><',
       function()
-        require('harpoon'):list():select(4)
+        require('harpoon'):list():prev()
       end,
       desc = '[T] Harpoon GoTo 2',
     },
@@ -82,15 +67,3 @@ return {
   branch = 'harpoon2',
   opts = {},
 }
-
--- vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
--- vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
---
--- vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
--- vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
--- vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
--- vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
---
--- -- Toggle previous & next buffers stored within Harpoon list
--- vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
--- vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
