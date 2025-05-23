04e4b33c1da01bc28d03d685bddd82b941bf8969
Simanga Khoza <simanga-dev@users.noreply.github.com>
2025-05-23 06:42:26 +0200

    hey

--- .local/share/bin/img2text.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-BASE_PATH="$HOME/Pictures/Screenshots/Text"
-
-grimblast save area "$BASE_PATH/$(date +%s).png"
-
-LATEST_IMG="$(exa --sort=modified $BASE_PATH | tail -1 )"
-
-tesseract "$BASE_PATH/$LATEST_IMG" stdout | wl-copy 
--- .local/share/bin/next-window.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-if  hyprctl activewindow 2>/dev/null | grep -q "floating: 0"; then
-    hyprctl dispatch cyclenext
-else
-    hyprctl dispatch layoutmsg cyclenext
-fi
